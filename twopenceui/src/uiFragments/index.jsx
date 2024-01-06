4ea5fe04fe1e3e7eae63382c199d4bf357d1a977
Innocent Josiah Patrick <innocentjosiah57@gmail.com>
2024-01-06 23:56:43 +0100

    about us almost done except for animation and bg images

--- twopenceui/src/uiFragments/index.jsx
+++ twopenceui/src/uiFragments/index.jsx
@@ -2,5 +2,7 @@ import AppHeader from "./AppHeader";
 import HeroSection from "./HeroSection";
 import AppSideBar from "./AppSideBar";
 import PoweredBy from "./PoweredBy";
+import HowToMint from "./HowToMint";
+import About from "./About";
 
-export { AppHeader, HeroSection, AppSideBar, PoweredBy };
+export { AppHeader, HeroSection, AppSideBar, PoweredBy, HowToMint, About };
